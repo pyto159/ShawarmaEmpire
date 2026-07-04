c14367586e02d9908157f46f2dcc5d11e422b149
pyto159 <75883148+pyto159@users.noreply.github.com>
2026-07-04 09:42:32 +0300

    Centralize economy balance config

--- shawarma-empire/Scripts/Recipes/Recipe.gd
+++ shawarma-empire/Scripts/Recipes/Recipe.gd
@@ -1,10 +1,8 @@
 extends Resource
 class_name Recipe
 
-const DEFAULT_BASE_PRICE: int = 0
 const DEFAULT_PREPARATION_TIME: float = 1.0
 
 @export var display_name: String = ""
-@export var base_price: int = DEFAULT_BASE_PRICE
 @export var preparation_time: float = DEFAULT_PREPARATION_TIME
 @export var required_ingredients: Array[Ingredient] = []
